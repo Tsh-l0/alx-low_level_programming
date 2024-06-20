18a9f43a753384d1af45753f2f27f6419fc3b1a9
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-06-20 11:57:25 -0700

    Write a new C programme that prints a line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * #include <stdio.h> includes the standard input/output library needed for the 'puts function'
+ * 
+ * 'int main(void)' The main function where the programme starts executing. It returns an integer value
+ *
+ * puts prints the phrase to the standard output and automatically adds a new line character at the end of the output.
+ *
+ * return(0) indicates the programme has ended successfully
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
