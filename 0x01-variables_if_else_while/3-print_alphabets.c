3d1e350de76d3f0ac460405aecc4723b6dbc475e
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-06-21 13:05:29 -0700

    lowercase UPPERCASE alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,18 +7,18 @@
 int main(void)
 {
 	char ch;
-	
+
 	/* This loops through the lowercase alphabet */
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch); /* Prints the lowercase alphabet */
 	}
-       	/* This loops through the uppercase alphabet */
+	/* This loops through the uppercase alphabet */
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch); /* Prints the uppercase alphabet */
 	}
 	putchar('\n'); /* Prints a new line */
-	
+
 	return (0);
 }
