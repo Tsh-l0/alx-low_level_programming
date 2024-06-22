b63ac92fedc2af6d2b31cbc61054fd90b98e6e39
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-06-22 02:06:06 -0700

    prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h> /* Initialize putchar function */
+
+/**
+ * main - Entry point
+ *
+ * Description Prints all possible combination of 1-digit numbers
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0; 
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
