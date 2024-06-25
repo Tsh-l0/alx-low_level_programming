0fe436ba71bd24ec78b88cf7760061c343f8afe3
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-06-25 15:25:43 -0700

    UPPER lower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 
 /**
  * _isalpha - Checks if a character is an uppercase/lowercase letter
- * @c - The character to check
+ * @c: The character to check
  *
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ? 1 : 0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ? 1 : 0);
 }
