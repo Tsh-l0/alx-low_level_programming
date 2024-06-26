8a08edc1a98289df3b20824db5946a82b3acdd88
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-06-25 22:51:42 -0700

    Show number signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
-/** main - Entry point
+/**
+ * main - Entry point
  *
- * print_sign - Prints the sign of a number
+ * print_sign: Prints the sign of a number
  * @n: The number to check
  *
- * Return: 1 and prints '+' if n is greater than zero
- * 	0 and prints '0' if n is zero
- * 	-1 and prints '-' if n is less than zero
+ * Return: 1 and prints '+' if n is greater than zero, 0 and prints '0' if n is zero, -1 and prints '-' if n is less than zero
  */
 int print_sign(int n)
 {
