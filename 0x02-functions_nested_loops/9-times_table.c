a7e9b99e5a7582171740123e4b5835e19a65178c
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-06-26 18:30:26 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,40 +7,29 @@ void times_table(void)
 {
 	int row, column, result;
 
-	for (row = 0; row <= 9; row++)
+	for (row = 0; row < 10; row++)
 	{
-		for (column = 0; column <= 9; column++)
+		for (column = 0; column < 10; column++)
 		{
-			result = row * column;
-
-			/* Print the result with properformatting */
+			result = column * row;
 			if (column == 0)
 			{
-				_putchar('0');
+				_putchar(result + '0');
+			}
+
+			if (result < 10 && column != 0)
+			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
 			}
-			else
+			else if (result >= 10)
 			{
-				if (result < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-
-				else
-				{
-					_putchar(' ');
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-
-				if (column < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
 		}
 		_putchar('\n');
