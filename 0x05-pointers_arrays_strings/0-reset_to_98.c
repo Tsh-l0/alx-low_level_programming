31cd65a2b5361fc8f12fbc1c0967d449a989e73e
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-07-01 11:55:19 -0700

    Take an int and point to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,9 +4,9 @@
  * reset_to_98 - Takes a pointer into an int and updates
  * the value it points to98
  *
- * @n: The integer 
+ * @n: The integer
  *
- * Return: 0 Always 
+ * Return: 0 Always
  */
 void reset_to_98(int *n)
 {
