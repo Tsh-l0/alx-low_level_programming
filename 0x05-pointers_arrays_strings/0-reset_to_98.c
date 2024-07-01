8957972f9866a1ce8750e739be63ad0f9e33fd7f
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-07-01 11:53:50 -0700

    Take an int and point to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Takes a pointer into an int and updates
+ * the value it points to98
+ *
+ * @n: The integer 
+ *
+ * Return: 0 Always 
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,32 @@
+/* Takeint as a parameter and update to 98 */
+void reset_to_98(int *n);
+
+/* Swop the value of two integers */
+void swap_int(int *a, int *b);
+
+/* Return the length of a string */
+int _strlen(char *s);
+
+/* Print a string to stdout */
+void _puts(char *str);
+
+/* Print a string in reverse */
+void print_rev(char *s);
+
+/* Reverse a string */
+void rev_string(char *s);
+
+/* Print every other character starting with the first character */
+void puts2(char *str);
+
+/* Print half a string */
+void puts_half(char *str);
+
+/* Print n elements of an array of integers */
+void print_array(int *a, int n);
+
+/* copy the string pointed by src */
+char *_strcpy(char *dest, char *src);
+
+/* putchar */
+int _putchar (char c);
