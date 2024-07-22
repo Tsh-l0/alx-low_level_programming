5ab08a1e7ffbaa6c6cdc6be0cd1f1e5e09caad98
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-07-22 13:40:39 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A C structure for dog name, age and owner
+ *
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
