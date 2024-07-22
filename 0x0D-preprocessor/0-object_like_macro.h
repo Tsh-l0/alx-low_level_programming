66f18e1a495fb1eac84f2d92c1e4f62d3bc4e41d
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-07-22 13:01:44 -0700

    Macro with abbreviation for 1024

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 1024_OBJECT_LIKE_MACRO_H__
-#define 1024_OBJECT_LIKE_MACRO_H__
+#ifndef 1024_OBJECT_LIKE_MACRO_H_
+#define 1024_OBJECT_LIKE_MACRO_H_
 
 #define SIZE 1024
 
-#endif /* 1024_OBJECT_LIKE_MACRO_H__ */
+#endif 
