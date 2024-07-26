0b246e0138f950166379a9a60a1d183207a58cb4
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-07-26 14:18:01 -0700

    a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Returns the sum of a and b
+ *
+ * @a: The first integer in the sum
+ * @b: The second integer in the sum
+ *
+ * Return: The sum of and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the difference between a and b
+ *
+ * @a: The first integer in the sum
+ * @b: The second integer in the sum
+ *
+ * Return: The difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the multiplication of a and b
+ *
+ * @a: The first integer in the sum
+ * @b: The second integer in the sum
+ *
+ * Return: The multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Returns the result of the division of a and b
+ *
+ * @a: The first integer in the sum
+ * @b: The second integer in the sum
+ *
+ * Return: The result of the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the remainder of the division of a and b
+ *
+ * @a: The first integer in the sum
+ * @b: The second integer in the sum
+ *
+ * Return: The remainder of the division of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
