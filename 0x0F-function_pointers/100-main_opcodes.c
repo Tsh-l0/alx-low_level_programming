c8dc6e222c1e87341a7cd5a8272fcb295109f745
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-07-26 14:30:42 -0700

    Generate opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints own opcodes.
+ * @argc: The number of arguments
+ * @argv: The array of arguments
+ * Returns: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int index, nbytes;
+	char *ptr = (char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < nbytes; index++)
+	{
+		printf("%02x", ptr[index] & 0xFF);
+		if (index != nbytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
