9b399549015c604b15552d504d91530c7cd6ab35
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-08-05 20:46:31 -0700

    Prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,13 +12,15 @@ size_t print_listint(const listint_t *h)
 {
 	size_t n_counter = 0;
 
-	/* Prints the value of a node */
+	/* Loops through the list as long as the list is not empty */
 	while (h != NULL)
 	{
+		/* Prints the value stored in the node */
 		printf("%d\n", h->n);
 
-		h = h->next;
+		/* Counts the nodes that have been printed */
 		n_counter++;
+		h = h->next;
 	}
 
 	return (n_counter);
