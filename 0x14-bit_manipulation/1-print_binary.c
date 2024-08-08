df98b2fa1c169b3e91c33181afe3475827636a67
Tsh-l0 <kodingwithtsh0l0@gmail.com>
2024-08-08 03:04:18 -0700

    prints the binary representation of a number.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary form of a number
+ *
+ * @n: The number to print in binary form
+ *
+ * Return: Void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int bit_mask = 1UL << (sizeof(n) * 8 - 1);
+	int track_bits = 0;
+
+	/* When n = 0 */
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	
+	/* Skip leading 0s */
+	while (bit_mask > n)
+		bit_mask >>= 1;
+	
+	/* Print each bit */
+	while (bit_mask > 0)
+	{
+		if (n & bit_mask)
+		{
+			putchar('1');
+			track_bits = 1;
+		}
+		else if (track_bits)
+		{
+			putchar('0');
+		}
+
+		bit_mask >>= 1;
+	}
+}
